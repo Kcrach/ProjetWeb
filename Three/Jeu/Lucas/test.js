9192749d61de83adcbec85002d4f24e6eb36b5de
Eno <47607638+Enoooooo@users.noreply.github.com>
2019-03-23 20:52:46 +0100

    Update test.js

--- Three/Jeu/Lucas/test.js
+++ Three/Jeu/Lucas/test.js
@@ -4,6 +4,15 @@ var height = 700;
 var mapWidth = 7;
 var mapHeight = 7;
 
+
+var nbmurs = 6;
+var listemurs = [];
+var nbflaques = 4;
+var listeflaques = [];
+
+// définit si il y a un obstacle en face du joueur ou non
+var blocked = false;
+
 /*
 Directions :
 	GAUCHE = 0
@@ -13,11 +22,18 @@ Directions :
 */
 var direction = 0; // direction regardée
 var rotDir = 0; // 0, rotation à gauche, 1=rotation à droite
-var moving = false;
-var rot = false;
+
 // bool pour arrêt des animations
+var moving = false;
+var rot = false; 
+// compteur pour les animations
 var moveCpt = 0;
 var rotCpt = 0; 
+
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
 
 function init() {
 	//scene et rendu
@@ -31,19 +47,77 @@ function init() {
 	//lumieres
 	var ambient = new THREE.AmbientLight({color: 0xffffff});
 	var dlight = new THREE.DirectionalLight({color: 0xffffff});
-	dlight.position.set(8, 35, 13);
+	dlight.position.set(8, 15, 13);
 
-	//objets
+	/* objets */
+	// joueur
 	var geometry = new THREE.BoxGeometry(0.8,0.8,0.8);
 	var material = new THREE.MeshPhongMaterial({color: 0xaa0000});
 	var cube = new THREE.Mesh(geometry, material);
 	
+	// sol
 	var plan = new THREE.BoxGeometry(mapWidth,1,mapHeight);
-	var solmat = new THREE.MeshPhongMaterial({color: 0x0000ff});
+	var solmat = new THREE.MeshPhongMaterial({color: 0x0000aa});
 	var sol = new THREE.Mesh(plan, solmat);
+	
+	// tous les murs, aleatoirement mais pas aux limites de la map
+	for(var i=0; i<nbmurs ; i++) {
+		var wall = new THREE.BoxGeometry(1,1,1);
+		var wallmat = new THREE.MeshPhongMaterial({color: 0x888888});
+		var mur = new THREE.Mesh(wall, wallmat);
+		
+		do {
+			var ok = true;
+			var x = Math.floor(Math.random() * Math.floor(mapWidth-2)) +1;
+			var z = Math.floor(Math.random() * Math.floor(mapHeight-2)) +1;	
+			listemurs.forEach(function(element) {
+				if(x == element.x && z == element.y) {
+					ok = false;
+				}
+			});
+		}
+		while(!ok);
+		
+		listemurs.push(new Point(x,z));
+		
+		mur.position.set(x,0,z);
+		scene.add(mur);
+	}
+	
+	// flaques magiques
+	for(var i=0; i<nbflaques ; i++) {
+		var cylindre = new THREE.CylinderGeometry(0.5,0.5,0.2,20,32);
+		var flaquemat = new THREE.MeshPhongMaterial({color: 0x00aa00});
+		var flaque = new THREE.Mesh(cylindre, flaquemat);
+		
+		do {
+			var ok = true;
+			var x = Math.floor(Math.random() * Math.floor(mapWidth));
+			var z = Math.floor(Math.random() * Math.floor(mapHeight));
+			
+			listeflaques.forEach(function(element) {
+				if(x == element.x && z == element.y) {
+						ok = false;
+				}
+			});
+			listemurs.forEach(function(element) {
+				if(x == element.x && z == element.y) {
+						ok = false;
+				}
+			});
+			
+		}
+		while(!ok);
+		
+		listeflaques.push(new Point(x,z));
+		
+		flaque.position.set(x,0,z);
+		scene.add(flaque);
+	}
 	
 	//ajouts et positionnements
 	scene.add(sol);
+	scene.add(mur);
 	scene.add(cube);
 	scene.add(ambient);
 	scene.add(dlight);
@@ -61,12 +135,20 @@ function init() {
 	
 	// position aléatoire sur le plateau
 	function randomPosition() {
-		var x = Math.floor(Math.random() * Math.floor(mapWidth));
+		do {
+			var ok = true;
+			var x = Math.floor(Math.random() * Math.floor(mapWidth));
+			var z = Math.floor(Math.random() * Math.floor(mapHeight));
+			listemurs.forEach(function(element) {
+				if(x == element.x && z == element.y) {
+					ok = false;
+				}
+			});
+		} while(!ok);
+		
 		cube.position.x = x;
-		
-		var z = Math.floor(Math.random() * Math.floor(mapHeight));
-		cube.position.z = z;	
-		
+		cube.position.z = z;
+				
 		direction = Math.floor(Math.random() * Math.floor(4));
 		
 		// réglages cam
@@ -79,6 +161,7 @@ function init() {
 		debugDir();
 		console.log("Position :"); console.log(cube.position);
 		console.log("\n");
+		checkCol();
 	}	
 	
 	function animate(){
@@ -136,6 +219,9 @@ function init() {
 			moveCpt = 0;
 			console.log(cube.position);
 			console.log("\n");
+			
+			checkTeleportFlaque();
+			checkCol();
 		}
 		else requestAnimationFrame(move);			
 	}
@@ -160,14 +246,76 @@ function init() {
 			debugDir();
 			rot = false;
 			rotCpt = 0;
+			
+			checkCol();
 		}
 		else requestAnimationFrame(rotate);			
 	}
 	
+	// met à jour le booléen blocked 
+	// en fonction de la présence ou non d'un mur face au joueur
+	function checkCol() {
+		var p = new Point();
+		switch(direction) {
+			case 0:
+				p.x = cube.position.x - 1;
+				p.y = cube.position.y;
+				p.z = cube.position.z; 
+				break;
+			case 1:
+				p.x = cube.position.x;
+				p.y = cube.position.y;
+				p.z = cube.position.z - 1; 
+				break;
+			case 2:
+				p.x = cube.position.x + 1;
+				p.y = cube.position.y;
+				p.z = cube.position.z; 
+				break;
+			case 3:
+				p.x = cube.position.x;
+				p.y = cube.position.y;
+				p.z = cube.position.z + 1; 
+				break;
+		}
+		
+		blocked = false;
+		listemurs.forEach(function(element) {
+			if(p.x == element.x && p.z == element.y) {
+				console.log(element);
+				blocked = true;
+			}
+		});
+	}
+	
+	// vérifie si l'on marche sur un flaque, téléporte si besoin
+	function checkTeleportFlaque() {
+		var ok = false;
+		var e;
+		listeflaques.forEach(function(element) {
+			if(cube.position.x == element.x && cube.position.z == element.y) {
+				ok = true;
+				e = element;
+			}
+		});
+		if(ok) {
+			do {
+				var random = Math.floor(Math.random() * Math.floor(nbflaques));
+				var f = listeflaques[random];
+			}
+			while(f == e);
+			
+			cube.position.x = f.x;
+			cube.position.z = f.y;
+			cam.position.x = f.x;
+			cam.position.z = f.y;
+		}
+	}
+	
 	// modulo fonctionnant sur les negatifs
 	function mod(n, m) {
-		var remain = n % m;
-		return Math.floor(remain >= 0 ? remain : remain + m);
+		var reste = n % m;
+		return Math.floor(reste >= 0 ? reste : reste + m);
 	};
 	
 	// fonction corrigeant le pb de precision des flottants
@@ -183,13 +331,13 @@ function init() {
 		if(!moving && !rot) {
 			switch(e.code) {
 				case "ArrowUp":
-					console.log("Nouveau déplacement");
-					console.log(cube.position);
-					move();
+					if(!blocked) {
+						console.log("Nouveau déplacement");
+						console.log(cube.position);
+						move();
+					}
 					break;
-				/*
-				case "ArrowDown":
-				
+				/*case "ArrowDown":
 					break;
 				*/
 				case "ArrowLeft":
@@ -229,11 +377,3 @@ function init() {
 	}
 
 }
-
-  
-	
-  
-
-
-
-
